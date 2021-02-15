d50a174613e5393bf53de7a97e0bfd3911d36fdb
hossain-shahrier <hossainshahrier.shm@gmail.com>
2021-02-15 16:22:11 +0600

    Area and Perimeter Calculator

---
+++ MID_LAB_TASK_PHP_04/area.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Area</title>
+</head>
+<body>
+    <?php  
+ $length =145;  
+ $width = 122;  
+ echo "Area of rectangle is $length * $width= " . ($length * $width) . "<br />";  
+ echo "Perimeter rectangle is 2* ($length + $width) =". (2*($length + $width));
+  ?>
+</body>
+</html>
+
