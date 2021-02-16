284db319bacecaa881454fd3daebf43795d147ee
hossain-shahrier <hossainshahrier.shm@gmail.com>
2021-02-16 11:42:59 +0600

    Task_2 Task_A Commit

---
+++ MID_LAB_TASK_PHP_05/Task_2/Task_A/email.php
@@ -0,0 +1,16 @@
+<?php
+	if(isset($_POST['submit'])){
+
+		$email =  $_POST['email'];
+		
+
+		if($email == "" ){
+			echo "Null submission...";
+		}else{
+			echo "Success!!!";
+		}
+
+	}else{
+		echo 'invalid request...';
+	}
+
